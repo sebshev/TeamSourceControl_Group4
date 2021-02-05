0f0fd1ef09c22284d7a6f488094ce94804ff6e85
Sebastian Shevlin <seb112697@gmail.com>
2021-02-04 20:38:19 -0800

    Created design for form

--- TeamSourceControl_Group4/Form1.Designer.cs
+++ TeamSourceControl_Group4/Form1.Designer.cs
@@ -29,13 +29,124 @@ namespace TeamSourceControl_Group4
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.addGameBtn = new System.Windows.Forms.Button();
+            this.deleteGameBtn = new System.Windows.Forms.Button();
+            this.gameLstBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // addGameBtn
+            // 
+            this.addGameBtn.Location = new System.Drawing.Point(101, 344);
+            this.addGameBtn.Name = "addGameBtn";
+            this.addGameBtn.Size = new System.Drawing.Size(112, 23);
+            this.addGameBtn.TabIndex = 0;
+            this.addGameBtn.Text = "Add Game";
+            this.addGameBtn.UseVisualStyleBackColor = true;
+            // 
+            // deleteGameBtn
+            // 
+            this.deleteGameBtn.Location = new System.Drawing.Point(407, 344);
+            this.deleteGameBtn.Name = "deleteGameBtn";
+            this.deleteGameBtn.Size = new System.Drawing.Size(75, 23);
+            this.deleteGameBtn.TabIndex = 1;
+            this.deleteGameBtn.Text = "Delete Game";
+            this.deleteGameBtn.UseVisualStyleBackColor = true;
+            // 
+            // gameLstBox
+            // 
+            this.gameLstBox.FormattingEnabled = true;
+            this.gameLstBox.Location = new System.Drawing.Point(370, 93);
+            this.gameLstBox.Name = "gameLstBox";
+            this.gameLstBox.Size = new System.Drawing.Size(157, 147);
+            this.gameLstBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Title:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Rating";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Price:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(138, 147);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(138, 208);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gameLstBox);
+            this.Controls.Add(this.deleteGameBtn);
+            this.Controls.Add(this.addGameBtn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button addGameBtn;
+        private System.Windows.Forms.Button deleteGameBtn;
+        private System.Windows.Forms.ListBox gameLstBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
