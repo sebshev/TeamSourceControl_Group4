f943b8e36f1863ccce2b5a751d90de526a152e7c
hm9811 <hm981198@gmail.com>
2021-03-12 14:42:34 -0800

    Added the button for update, and added code to prove the update function is working.

--- TeamSourceControl_Group4/Form1.Designer.cs
+++ TeamSourceControl_Group4/Form1.Designer.cs
@@ -38,6 +38,7 @@ namespace TeamSourceControl_Group4
             this.gameLstBox = new System.Windows.Forms.ListBox();
             this.deleteGameBtn = new System.Windows.Forms.Button();
             this.addGameBtn = new System.Windows.Forms.Button();
+            this.updateGameBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ratingCmbBxox
@@ -47,66 +48,68 @@ namespace TeamSourceControl_Group4
             "Everyone",
             "Teen",
             "Mature"});
-            this.ratingCmbBxox.Location = new System.Drawing.Point(241, 205);
+            this.ratingCmbBxox.Location = new System.Drawing.Point(281, 189);
             this.ratingCmbBxox.Name = "ratingCmbBxox";
-            this.ratingCmbBxox.Size = new System.Drawing.Size(100, 21);
+            this.ratingCmbBxox.Size = new System.Drawing.Size(116, 20);
             this.ratingCmbBxox.TabIndex = 18;
             // 
             // priceTxt
             // 
-            this.priceTxt.Location = new System.Drawing.Point(241, 144);
+            this.priceTxt.Location = new System.Drawing.Point(281, 133);
             this.priceTxt.Name = "priceTxt";
-            this.priceTxt.Size = new System.Drawing.Size(100, 20);
+            this.priceTxt.Size = new System.Drawing.Size(116, 21);
             this.priceTxt.TabIndex = 17;
             // 
             // titleTxt
             // 
-            this.titleTxt.Location = new System.Drawing.Point(241, 86);
+            this.titleTxt.Location = new System.Drawing.Point(281, 79);
             this.titleTxt.Name = "titleTxt";
-            this.titleTxt.Size = new System.Drawing.Size(100, 20);
+            this.titleTxt.Size = new System.Drawing.Size(116, 21);
             this.titleTxt.TabIndex = 16;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(171, 147);
+            this.label3.Location = new System.Drawing.Point(199, 136);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.Size = new System.Drawing.Size(38, 12);
             this.label3.TabIndex = 15;
             this.label3.Text = "Price:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(171, 205);
+            this.label2.Location = new System.Drawing.Point(199, 189);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.Size = new System.Drawing.Size(44, 12);
             this.label2.TabIndex = 14;
             this.label2.Text = "Rating:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(171, 89);
+            this.label1.Location = new System.Drawing.Point(199, 82);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.Size = new System.Drawing.Size(33, 12);
             this.label1.TabIndex = 13;
             this.label1.Text = "Title:";
             // 
             // gameLstBox
             // 
             this.gameLstBox.FormattingEnabled = true;
-            this.gameLstBox.Location = new System.Drawing.Point(473, 90);
+            this.gameLstBox.ItemHeight = 12;
+            this.gameLstBox.Location = new System.Drawing.Point(454, 65);
             this.gameLstBox.Name = "gameLstBox";
-            this.gameLstBox.Size = new System.Drawing.Size(157, 147);
+            this.gameLstBox.Size = new System.Drawing.Size(182, 136);
             this.gameLstBox.TabIndex = 12;
+            this.gameLstBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.gameLstBox_MouseClick);
             this.gameLstBox.SelectedIndexChanged += new System.EventHandler(this.gameLstBox_SelectedIndexChanged);
             // 
             // deleteGameBtn
             // 
-            this.deleteGameBtn.Location = new System.Drawing.Point(510, 341);
+            this.deleteGameBtn.Location = new System.Drawing.Point(595, 315);
             this.deleteGameBtn.Name = "deleteGameBtn";
-            this.deleteGameBtn.Size = new System.Drawing.Size(75, 23);
+            this.deleteGameBtn.Size = new System.Drawing.Size(87, 21);
             this.deleteGameBtn.TabIndex = 11;
             this.deleteGameBtn.Text = "Delete Game";
             this.deleteGameBtn.UseVisualStyleBackColor = true;
@@ -114,19 +117,30 @@ namespace TeamSourceControl_Group4
             // 
             // addGameBtn
             // 
-            this.addGameBtn.Location = new System.Drawing.Point(204, 341);
+            this.addGameBtn.Location = new System.Drawing.Point(238, 315);
             this.addGameBtn.Name = "addGameBtn";
-            this.addGameBtn.Size = new System.Drawing.Size(112, 23);
+            this.addGameBtn.Size = new System.Drawing.Size(131, 21);
             this.addGameBtn.TabIndex = 10;
             this.addGameBtn.Text = "Add Game";
             this.addGameBtn.UseVisualStyleBackColor = true;
             this.addGameBtn.Click += new System.EventHandler(this.addGameBtn_Click);
             // 
+            // updateGameBtn
+            // 
+            this.updateGameBtn.Location = new System.Drawing.Point(420, 315);
+            this.updateGameBtn.Name = "updateGameBtn";
+            this.updateGameBtn.Size = new System.Drawing.Size(113, 21);
+            this.updateGameBtn.TabIndex = 19;
+            this.updateGameBtn.Text = "Update";
+            this.updateGameBtn.UseVisualStyleBackColor = true;
+            this.updateGameBtn.Click += new System.EventHandler(this.updateGameBtn_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(933, 415);
+            this.Controls.Add(this.updateGameBtn);
             this.Controls.Add(this.ratingCmbBxox);
             this.Controls.Add(this.priceTxt);
             this.Controls.Add(this.titleTxt);
@@ -154,6 +168,7 @@ namespace TeamSourceControl_Group4
         private System.Windows.Forms.ListBox gameLstBox;
         private System.Windows.Forms.Button deleteGameBtn;
         private System.Windows.Forms.Button addGameBtn;
+        private System.Windows.Forms.Button updateGameBtn;
     }
 }
 
